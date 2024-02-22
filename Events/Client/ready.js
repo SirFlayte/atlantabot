665f27c236f74d09f84cba493bf2243c74162563
SirFlayte <designerwalim@gmail.com>
2024-02-22 16:35:50 +0100

    Event handler

---
+++ Events/Client/ready.js
@@ -0,0 +1,8 @@
+const { Client } = require('discord.js')
+module.exports = {
+    name: "ready",
+    once: true,
+    execute(client) {
+        console.log(`${client.user.tag} is now online.`)
+    }
+}
