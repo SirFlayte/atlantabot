253d053d523a501367146e396cfb39b630b8720f
SirFlayte <designerwalim@gmail.com>
2024-03-03 11:24:32 +0100

    multi guild welcome message

--- Events/Guild/guildMemberAdd.js
+++ Events/Guild/guildMemberAdd.js
@@ -1,23 +1,31 @@
 const {EmbedBuilder} = require('@discordjs/builders');
 const {GuildMember} = require("discord.js");
 const {welcomeId, memberId} = require('../../config.json')
+const {Schema} = require("../../Models/Welcome");
 
 module.exports = {
     name: "guildMemberAdd",
     once: false,
     execute(member) {
-        const {user, guild} = member;
-        const welcomeChannel = member.guild.channels.cache.get(welcomeId);
-        const welcomeMessage = `Welcome <@${member.id}> to the guild!`;
+        Schema.findOne({Guild: member.guild.id}, async (err, data) => {
+            if (!data) return;
 
-        const welcomeEmbed = new EmbedBuilder()
-            .setTitle("**New member!**")
-            .setDescription(welcomeMessage)
-            .setColor(0x037821)
-            .setTimestamp()
-            .addFields({name: "Total members", value: `${guild.memberCount}`});
+            let channel = data.Channel;
+            let Msg = data.Msg || "";
+            let Role = data.role;
 
-        welcomeChannel.send({embeds: [welcomeEmbed]});
-        member.roles.add(memberId);
+            const {user, guild} = member;
+            const welcomeChannel = member.guild.channels.cache.get(channel)
+
+            const welcomeEmbed = new EmbedBuilder()
+                .setTitle("**New member!**")
+                .setDescription(Msg)
+                .setColor(0x037821)
+                .setTimestamp()
+                .addFields({name: "Total members", value: `${guild.memberCount}`});
+
+            welcomeChannel.send({embeds: [welcomeEmbed]});
+            member.roles.add(Role);
+        })
     },
 };
